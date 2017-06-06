7e74bbe536a4f3c5eaf24b946d6568f2d7e75743
Mehdi <mehdi@lajavaness.com>
2017-06-06 19:40:15 +0200

    Write function in bridges and add function in solidity, up README

--- API/contracts/XtremWebInterface.sol
+++ API/contracts/XtremWebInterface.sol
@@ -2,64 +2,73 @@ pragma solidity ^0.4.11,
 
 contract XtremWebInterface {
 
-        address public bridge;
+    address public bridge;
 
-        function XtremWebInterface() {
-            bridge = msg.sender;
-        }
+    function XtremWebInterface() {
+        bridge = msg.sender;
+    }
 
-        enum Status {Completed, Pending, Running, Waiting, Error}
+    enum Status {Completed, Pending, Running, Waiting, Error}
 
+    /*
+     * XWObject aims is to reproduce XWEB Object in solidity
+     */
 		struct XWObject {
 			string name;        // application name
 			string timestamp;
 			Status status;
 			string stdout;
 			string pattern;
+      param[] params;
 		}
-
 		mapping (address => mapping (uint => XWObject)) public workRegisteries;
+    mapping (string => string) public param;
 
-
+    /*
+     * LAUNCHER FUNCTIONS
+     * These functions are launcher functions, the launch Event wich are called by the bridge,
+     * and then the bridge call XTREMweb job, wait for result then modify the smart contract.
+     */
 		function register(string appName) {
-            Launch(msg.sender,"register", appName, "", 0);
+        Launch(msg.sender,"register", appName, "", "", 0);
 		}
-
-		// param = commandline
-		function submit(string appName, string param) {
-			Launch(msg.sender,"submit", param, "", 0);
+		function submit(string appName, string param) { 		// param = commandline
+        Launch(msg.sender,"submit", param, "", "", 0);
 		}
-
-		function getStatus() returns (Status) {
-		    return workRegisteries[msg.sender][0].status;
-		}
-
 		function submitAndWait(string appName, string param, string pattern) {
-		    Launch(msg.sender,"submitAndWait", pattern, "", 0);
+		    Launch(msg.sender,"submitAndWait", pattern, "", "", 0);
 		}
 		function setParam(uint UID, string paramName, string paramValue) {
-		    Launch(msg.sender,"setParam", paramName, paramValue, UID);
+		    Launch(msg.sender,"setParam", paramName, paramValue, "", UID);
 		}
 		function setPending(uint UID) {
 		    Launch(msg.sender,"setParam", "status", "pending", UID);
 		}
 		function status(uint UID) {
-		    Launch(msg.sender,"status", "", "", UID);
+		    Launch(msg.sender,"status", "", "", "", UID);
 		}
 		function result(uint UID) {
-		    Launch(msg.sender,"result", "", "", UID);
+		    Launch(msg.sender,"result", "", "", "", UID);
 		}
 		function stdout(uint UID) {
-		    Launch(msg.sender,"stdout", "", "", UID);
+		    Launch(msg.sender,"stdout", "", "", "", UID);
 		}
 		function toDelete(uint UID) {
-		    Launch(msg.sender,"toDelete", "", "", UID);
+		    Launch(msg.sender,"toDelete", "", "", "", UID);
 		}
 		function waitResult(uint UID, string pattern) {
-            Launch(msg.sender,"waitResult", pattern, "", UID);
+        Launch(msg.sender,"waitResult", pattern, "", "", UID);
 		}
 
-        // only by bridge
+    // GETTER
+    function getParam(uint UID, string paramName) {
+        Launch(msg.sender, "getParam")
+    }
+
+    /*
+     * ONLY BY BRIDGE
+     * The following functions are called only by the bridge, to modify the state of the XWObject
+     */
 		function addUID(address owner, uint _UID, string _appName, string _timestamp, Status _status) onlyBy(bridge) {
 		    //workRegisteries[owner].push(XWObject({UID : _UID, name : _appName, timestamp : _timestamp, status : _status, stdout: "", pattern: ""}));
 		    workRegisteries[owner][_UID].name = _appName;
@@ -67,15 +76,22 @@ contract XtremWebInterface {
 		    workRegisteries[owner][_UID].status = _status;
 		}
 
-        function setStdout(uint UID, string _stdout, address owner) onlyBy(bridge) {
+    function setStdout(uint UID, string _stdout, address owner) onlyBy(bridge) {
+    }
 
-        }
+    function pushResult(uint UID, address owner, string _stdout) onlyBy(bridge) {
+      workRegisteries[owner][_UID].stdout = _stdout;
+      workRegisteries[owner][_UID].status = Status.Completed;
+    }
 
-        event Launch( address indexed user, string functionName, string param1, string param2, uint UID); // special log to launch process
-        event Logs(string status, address indexed user); // logs for the front-end or smart contract to react correctly
+    /*
+     * EVENTS AND MODIFIERS
+     */
+    event Launch( address indexed user, string functionName, string param1, string param2, string param3,uint UID); // special log to launch process
+    event Logs(string status, address indexed user); // logs for the front-end or smart contract to react correctly
 
- 	    modifier onlyBy(address a){
-	        if (msg.sender != a) throw;
-	        _;
-	    }
+ 	  modifier onlyBy(address a){
+	      if (msg.sender != a) throw;
+	      _;
+	  }
 }
