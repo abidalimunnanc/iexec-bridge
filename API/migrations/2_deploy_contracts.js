c191eb1120b0fbf38cb4535aef71f96c49940b50
francois branciard <francois.branciard@iex.ec>
2017-08-28 23:41:31 +0200

    Fix migrate script

--- API/migrations/2_deploy_contracts.js
+++ API/migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
-var IexecWorksGateway = artifacts.require("./IexecWorksGateway.sol");
+var IexecOracle = artifacts.require("./IexecOracle.sol");
 var LS = artifacts.require("./LS.sol");
 
 
 module.exports = function(deployer) {
-  return deployer.deploy(IexecWorksGateway)
-    .then(() => IexecWorksGateway.deployed())
+  return deployer.deploy(IexecOracle)
+    .then(() => IexecOracle.deployed())
     .then(instance => {
-      console.log("IexecWorksGateway deployed at address :" + instance.address)
+      console.log("IexecOracle deployed at address :" + instance.address)
       return LS.new(instance.address);
     })
     .then(instance => console.log("LS deployed at address :" + instance.address));
