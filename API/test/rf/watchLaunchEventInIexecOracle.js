66ed0290a2d86028087b95623342d6076e1efdee
francois branciard <francois.branciard@iex.ec>
2017-09-06 12:02:18 +0200

    IEXPROD-34 : add npm run devbridgelog in package.json + WIP watch event test

--- API/test/rf/watchLaunchEventInIexecOracle.js
+++ API/test/rf/watchLaunchEventInIexecOracle.js
@@ -6,49 +6,52 @@ const Extensions = require("../../utils/extensions.js");
 const addEvmFunctions = require("../../utils/evmFunctions.js");
 addEvmFunctions(web3);
 Promise.promisifyAll(web3.eth, {
-    suffix: "Promise"
+  suffix: "Promise"
 });
 Promise.promisifyAll(web3.version, {
-    suffix: "Promise"
+  suffix: "Promise"
 });
 Promise.promisifyAll(web3.evm, {
-    suffix: "Promise"
+  suffix: "Promise"
 });
 Extensions.init(web3, assert);
 
 
 contract('IexecOracle', function(accounts) {
 
-    var creator, bridge, user, provider;
-    var amountGazProvided = 4000000;
-    let isTestRPC;
+  var creator, bridge, user, provider;
+  var amountGazProvided = 4000000;
+  let isTestRPC;
 
-    before("should prepare accounts and check TestRPC Mode", function() {
-        assert.isAtLeast(accounts.length, 4, "should have at least 4 accounts");
-        creator = accounts[0];
-        bridge = accounts[1];
-        user = accounts[2];
+  before("should prepare accounts and check TestRPC Mode", function() {
+    assert.isAtLeast(accounts.length, 4, "should have at least 4 accounts");
+    creator = accounts[0];
+    bridge = accounts[1];
+    user = accounts[2];
 
-        return Extensions.makeSureAreUnlocked(
-                [creator, bridge, user])
-                .then(() => web3.eth.getBalancePromise(creator))
-        .then(balance => assert.isTrue(
-            web3.toWei(web3.toBigNumber(90), "ether").lessThan(balance),
-            "creator should have at least 35 ether, not " + web3.fromWei(balance, "ether")))
-        .then(() => Extensions.refillAccount(creator, user, 30))
-        .then(() => Extensions.refillAccount(creator, bridge, 30))
-        .then(() => web3.version.getNodePromise())
-        .then(node => isTestRPC = node.indexOf("EthereumJS TestRPC") >= 0);
+    return Extensions.makeSureAreUnlocked(
+        [creator, bridge, user])
+      .then(() => web3.eth.getBalancePromise(creator))
+      .then(balance => assert.isTrue(
+        web3.toWei(web3.toBigNumber(90), "ether").lessThan(balance),
+        "creator should have at least 35 ether, not " + web3.fromWei(balance, "ether")))
+      .then(() => Extensions.refillAccount(creator, user, 30))
+      .then(() => Extensions.refillAccount(creator, bridge, 30))
+      .then(() => web3.version.getNodePromise())
+      .then(node => isTestRPC = node.indexOf("EthereumJS TestRPC") >= 0);
+  });
+
+
+  it("watch CallbackEvent", function() {
+    var aIexecOracleInstance;
+    return IexecOracle.deployed().then(instance => {
+      aIexecOracleInstance = instance;
+      return Extensions.getEventsPromise(aIexecOracleInstance.Launch({}, {
+        fromBlock: 0,
+        toBlock: "latest"
+      }),1);
+    }).then(events => {
+      console.log("events :" + events);
     });
-
-
-    it("watch CallbackEvent", function() {
-      var aIexecOracleInstance;
-      return IexecOracle.deployed().then(instance => {
-          aIexecOracleInstance = instance;
-        return Extensions.getEventsPromise(aIexecOracleInstance.Launch({}));
-      }).then(events => {
-        console.log("events :"+events);
-      });
-    });
+  });
 });
