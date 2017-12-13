fd7258bceccd756908f4be2d386d27693696236a
victor <victor@vbonhomme.fr>
2017-12-13 11:28:01 +0100

    refactor

--- src/app.js
+++ src/app.js
@@ -9,12 +9,13 @@ const { signAndSendTx, walletFromPrivKey } = require('./utils');
 const debug = Debug('app');
 
 const {
-  CHAIN, HOST, PRIVATE_KEY, XW_LOGIN, XW_PWD, XW_HOST, XW_PORT, IEXEC_ORACLE
+  CHAIN, HOST, PRIVATE_KEY, XW_LOGIN, XW_PWD, XW_HOST, XW_PORT, IEXEC_ORACLE,
 } = process.env;
 debug('CHAIN', CHAIN);
 
 const rlcWallet = walletFromPrivKey(PRIVATE_KEY);
 const { network_id } = config.networks[CHAIN];
+debug('network_id', network_id);
 
 const ws = new Web3.providers.WebsocketProvider(HOST);
 
@@ -23,14 +24,10 @@ ws.on('end', evt => debug('onEnd', evt));
 ws.on('error', error => debug('onError', error));
 
 const web3 = new Web3(ws);
-let oracleAddress='';
-if (oracleJSON.networks[network_id] == null){
-    oracleAddress=IEXEC_ORACLE;
-}
-else{
-    oracleAddress = oracleJSON.networks[network_id].address;
-}
+
+const oracleAddress = IEXEC_ORACLE || oracleJSON.networks[network_id].address;
 debug('watching oracle at ', oracleAddress);
+
 const oracleContract = new web3.eth.Contract(oracleJSON.abi, oracleAddress);
 const xwhep = createXWHEPClient({
   login: XW_LOGIN,
@@ -39,21 +36,34 @@ const xwhep = createXWHEPClient({
   port: XW_PORT,
 });
 
-oracleContract.events.Submit(async (error, event) => {
-  let submitTxHash='';
-  let dapp='';
-  let user='';
-  let provider='';
-  let args='';
-  try {
-    if (error) return debug('Submit error', error);
-    debug('Submit event', event);
-    user= event.returnValues.user;
-    provider= event.returnValues.provider;
-    args= event.returnValues.args;
-    dapp = event.returnValues.dapp.toLowerCase();
-    submitTxHash = event.transactionHash;
+async function callback(submitTxHash, user, dapp, status, stdout, stderr, uri) {
+  const unsignedTx = oracleContract.methods.submitCallback(
+    submitTxHash, user, dapp,
+    status, stdout, stderr, uri,
+  ).encodeABI();
+  debug('unsignedTx', unsignedTx);
 
+  const txReceipt = await signAndSendTx({
+    web3,
+    userWallet: rlcWallet,
+    unsignedTx,
+    nonceOffset: 0,
+    contractAddress: oracleAddress,
+    chainID: network_id,
+  });
+  debug('processed txReceipt', txReceipt);
+  return txReceipt;
+}
+
+oracleContract.events.Submit(async (error, event) => {
+  if (error) return debug('Submit error', error);
+
+  debug('Submit event', event);
+  const { user, provider, args } = event.returnValues;
+  const dapp = event.returnValues.dapp.toLowerCase();
+  const submitTxHash = event.transactionHash;
+
+  try {
     debug('args', args);
     let param = {};
     try {
@@ -74,25 +84,7 @@ oracleContract.events.Submit(async (error, event) => {
 
     return callback(submitTxHash, user, dapp, 4, stdout, '', uri);
   } catch (e) {
-     debug('error onSubmit', e);
-     return callback(submitTxHash, user, dapp, 5, '', 'Bridge failed. Off-chain computation cancelled', '');
+    debug('error onSubmit', e);
+    return callback(submitTxHash, user, dapp, 5, '', 'Bridge failed. Off-chain computation cancelled', '');
   }
 });
-
-
-
-async function callback(submitTxHash, user, dapp, status, stdout, stderr, uri){
-    const unsignedTx = oracleContract.methods.submitCallback(submitTxHash, user, dapp, status, stdout, stderr, uri).encodeABI();
-    debug('unsignedTx', unsignedTx);
-
-    const txReceipt = await signAndSendTx({
-        web3,
-        userWallet: rlcWallet,
-        unsignedTx,
-        nonceOffset: 0,
-        contractAddress: oracleAddress,
-        chainID: network_id,
-    });
-    debug('processed txReceipt', txReceipt);
-    return txReceipt;
-}
